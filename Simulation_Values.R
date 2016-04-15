5ba931af4dfef05b8df2f0a993476cdf4e0138a8
Lydia Beaudrot <lbeaudrot@conservation.org>
2016-04-15 12:05:56 -0700

    New files: functions and loops for simulation study

--- Simulation_Values.R
+++ Simulation_Values.R
@@ -1,26 +1,58 @@
-# Generate list of input values for simulation study where each item in the list has a vector of input values for
-# function(points,days,psi,p,phi,gamma,years)
+############ TEAM CT DYNAMIC OCCUPANCY SIMULATION STUDY ##############
 
-# Define parameter values of interest
+# Load functions for simulating data
+source(file="dynoFunctions.R")
+
+# Generate list of input values for simulation study where each item in the list has a vector of input values for
+# function(points, days, psi,p ,phi ,gamma, years, nsim)
 pts <- c(10, 20, 30, 60, 90, 120)
 days <- c(15, 30, 45, 60)
 psi <- c(0.5)
 p <- c(0.08, 0.2, 0.5)
 phi <- c(0.99, 0.95, 0.9, 0.85)
 g <- 0
-yrs <- c(10)
+yrs <- c(15)
+nsim <- c(500)
+# Create object with all combinations of input parameters
+mat <- expand.grid(pts, days, psi, p, phi, g, yrs, nsim)
+colnames(mat) <- c("pts", "days", "psi", "p", "phi", "g", "yrs", "nsim")
 
-mat <- expand.grid(pts, days, psi, p, phi, g, yrs)
-colnames(mat) <- c("pts", "days", "psi", "p", "phi", "g", "yrs")
-mat.list <- alply(.data=mat, .margins=1, fun="[")
+# Setting up the machine for parallel processing
+no_cores <- detectCores() - 1
+cl<-makeCluster(no_cores)
+registerDoParallel(cl)
+clusterSetRNGStream(cl=cl, iseed=400)
 
+# Run loop to generate simulations for each combination of parameters in object "mat"
+result <- list()
+for(i in 1:dim(mat)[1]){
+    test <- simulatetrend(points=mat[i,1], days=mat[i,2], psi=mat[i,3], p=mat[i,4], phi=mat[i,5], gamma=mat[i,6], years=mat[i,7], nsim=mat[i,8])
+    result[[i]] <- test
+    names(result)[i] <- paste("input values", paste(mat[i,], collapse=", "))
+}
 
-# If we are running the same version of R, set.seed should produce similar output, although if R installation differed, they could be different
-# see https://stat.ethz.ch/pipermail/r-help/2005-September/079391.html
+stopCluster(cl)
 
-# If parallelizing, we should NOT use set.seed, but should instead use the function clusterSetRNGStreatm() from the parallel package
-# see http://www.r-bloggers.com/how-to-go-parallel-in-r-basics-tips/
+# Run second loop to calculate changes for each set of simulation values
 
-# It doesn't matter what number we use for the seed
-# see http://www.r-bloggers.com/what-are-the-most-common-rng-seeds-used-in-r-scripts-on-github/
-# see also http://www.r-bloggers.com/a-look-at-random-seeds-in-r-or-85-why-cant-you-be-more-like-548/
+result2 <- list()
+y.first <- vector()
+
+for(i in 1:length(result)){
+    res <- data.frame(result[[i]])
+    slopes <- data.frame(slopes=res[,ncol(res)])
+    changes <- data.frame(res[,(ncol(res)/2+1):(ncol(res)-1)])
+    res <- res[,-c((ncol(res)/2+1):(ncol(res)))]
+    res <- data.frame(res, id = 1:nrow(res))
+    names(res) <- c(1:(ncol(res)-1), "id")
+    res <- melt(res,c("id"), variable_name = "year")
+    result2[[i]] <- res
+    names(result2)[i] <- names(result)[i]
+    names(changes) <- c(1:ncol(changes))
+    confint <- apply(changes,2,quantile,c(0.1,0.9))
+    y <- which(round(confint[1,],2) > 0)
+    y.first[i] <- ifelse(length(y)==0,0,min(y))
+}
+
+det.year <- data.frame(y.first=y.first, mat, input=names(result))
+
---
+++ dynoFunctions.R
@@ -0,0 +1,89 @@
+# DynOccFunctions
+library(unmarked)
+library(ggplot2)
+library(parallel)
+library(foreach)
+library(doParallel)
+
+gm_mean = function(x, na.rm=TRUE, zero.propagate = FALSE){
+  if(any(x < 0, na.rm = TRUE)){
+    return(NaN)
+  }
+  if(zero.propagate){
+    if(any(x == 0, na.rm = TRUE)){
+      return(0)
+    }
+    exp(mean(log(x), na.rm = na.rm))
+  } else {
+    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+  }
+}
+
+
+data.generator<-function(points,days,psi,p,phi,gamma,years) {
+        require(unmarked)
+        #first year of data
+        y1<-matrix(NA,nr=points,nc=days)
+        #generate the expected occupancies
+        z<-rbinom(points,1,psi)
+        #generate the observations
+        for(i in 1:points)
+                y1[i,]<-rbinom(days,1,z[i]*p)
+        #subsequent years
+        #three dimensional matrix to store the results
+        yk<-array(NA,dim=c(points,days,years))
+        yk[,,1]<-y1
+        for(k in 2:years){
+        #generate the deterministic part of the model
+                occ<-apply(yk[,,k-1],1,max,na.rm=T)
+                z<-rbinom(points,1,occ*phi+(1-occ)*gamma)
+        #generate the observations
+        for(i in 1:points)
+                yk[i,,k]<-rbinom(days,1,z[i]*p)
+     
+        }  
+        # convert results to a two dimensional matrix for colext
+        yk <- matrix(yk, points, days*years)
+        #ny <- matrix(as.character(1:years),nrow(yk), years, byrow=TRUE)
+        # store data in an unmarkedMultFrame object so it is ready for analysis
+        yUMF <- unmarkedMultFrame(y = yk, numPrimary = years)
+        yUMF
+} 
+
+
+
+simulatetrend <- function(points, days, psi, p, phi, gamma, years, nsim) {
+        #store results
+        #gm_lambda <- numeric()
+        time <- 1:years
+        results <- foreach(i = 1:nsim, .combine = rbind, .packages = "unmarked", .export = c("data.generator","gm_mean")) %dopar% {
+                #generate data and store projected results
+                data <- data.generator(points,days,psi,p,phi,gamma,years)
+                model <- colext(~1, ~1, ~1, ~1, data = data, method = "BFGS",se = FALSE)
+                timeseries <- as.numeric(smoothed(model)[2,])
+                
+                #calculate difference between year 1 and year n
+                diff1_n <- timeseries[1] - timeseries[-1]
+                
+                #Calculate geometric mean of lambda
+                lambda <- timeseries[2:years]/timeseries[1:(years-1)]
+                lambda <- -(gm_mean(lambda) - 1)
+                c(timeseries,diff1_n,lambda)
+        }
+        results
+        #list(gmlambda = gm_lambda, results = results)
+}
+
+
+# NOTES
+
+# If we are running the same version of R, set.seed should produce similar output, although if R installation differed, they could be different
+# see https://stat.ethz.ch/pipermail/r-help/2005-September/079391.html
+
+# If parallelizing, we should NOT use set.seed, but should instead use the function clusterSetRNGStreatm() from the parallel package
+# see http://www.r-bloggers.com/how-to-go-parallel-in-r-basics-tips/
+
+# It doesn't matter what number we use for the seed
+# see http://www.r-bloggers.com/what-are-the-most-common-rng-seeds-used-in-r-scripts-on-github/
+# see also http://www.r-bloggers.com/a-look-at-random-seeds-in-r-or-85-why-cant-you-be-more-like-548/
+
